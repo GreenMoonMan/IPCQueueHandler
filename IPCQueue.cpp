e743821d4b5dad7ccefe53dc73c401d80de0dbbf
moon <computer.local>
2024-03-23 15:44:48 -0700

    skeleton created

---
+++ IPCQueue.cpp
@@ -0,0 +1,27 @@
+#include "IPCQueue.h"
+
+
+IPCQueue::IPCQueue(const std::string IPCName)
+{
+
+}
+
+
+IPCQueue::~IPCQueue()
+{
+
+}
+
+
+std::string IPCQueue::read() const
+{
+
+}
+
+
+void IPCQueue::write(std::string command)
+{
+
+}
+
+
